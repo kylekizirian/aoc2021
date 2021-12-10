d80a4d6f7aec7769ab4f3671ca21e3a10ef23259
Kyle Kizirian <kyle.kizirian@gmail.com>
2021-12-10 15:20:46 -0500

    day 10 part 2

--- day10/main.go
+++ day10/main.go
@@ -4,12 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
+
+var closeToOpen = map[string]string{")": "(", "]": "[", "}": "{", ">": "<"}
 
 func main() {
 	lines := readInput()
 	part1(lines)
+	part2(lines)
 }
 
 func part1(lines []string) {
@@ -20,35 +24,57 @@ func part1(lines []string) {
 	fmt.Println("part 1: ", score)
 }
 
+func part2(lines []string) {
+	var scores []int
+	for _, line := range lines {
+		score := completionScore(line)
+		if score != 0 {
+			scores = append(scores, score)
+		}
+	}
+	sort.Ints(scores)
+	fmt.Println("part 2: ", scores[len(scores)/2])
+}
+
 func corruptedScore(line string) int {
+	charScore := map[string]int{")": 3, "]": 57, "}": 1197, ">": 25137}
 	var stack []string
 	for _, char := range strings.Split(line, "") {
 		switch char {
 		case "(", "[", "{", "<":
 			stack = append(stack, char)
-		case ")":
-			if stack[len(stack)-1] != "(" {
-				return 3
-			}
-			stack = stack[:len(stack)-1]
-		case "]":
-			if stack[len(stack)-1] != "[" {
-				return 57
-			}
-			stack = stack[:len(stack)-1]
-		case "}":
-			if stack[len(stack)-1] != "{" {
-				return 1197
-			}
-			stack = stack[:len(stack)-1]
-		case ">":
-			if stack[len(stack)-1] != "<" {
-				return 25137
+		default:
+			if stack[len(stack)-1] != closeToOpen[char] {
+				return charScore[char]
 			}
 			stack = stack[:len(stack)-1]
 		}
 	}
 	return 0
+}
+
+func completionScore(line string) int {
+	charScore := map[string]int{"(": 1, "[": 2, "{": 3, "<": 4}
+	var stack []string
+	for _, char := range strings.Split(line, "") {
+		switch char {
+		case "(", "[", "{", "<":
+			stack = append(stack, char)
+		default:
+			if stack[len(stack)-1] != closeToOpen[char] {
+				return 0
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	score := 0
+	for i := len(stack) - 1; i >= 0; i-- {
+		score *= 5
+		score += charScore[stack[i]]
+	}
+
+	return score
 }
 
 func readInput() []string {
