db3aaf48757ab90c6d5bd5af606f27bd771147fe
Kyle Kizirian <kyle.kizirian@gmail.com>
2021-12-15 17:10:23 -0500

    day 15 part 2

--- day15/main.go
+++ day15/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -12,33 +12,155 @@ import (
 func main() {
 	grid := readInput()
 	part1(grid)
+	part2(grid)
 }
 
 func part1(grid [][]int) {
-	minRisk := make([][]int, len(grid))
-	for i := range minRisk {
-		minRisk[i] = make([]int, len(grid[i]))
-	}
+	fmt.Println("part 1: ", minRisk(grid))
+}
 
-	for i := range minRisk {
-		for j := range minRisk[i] {
-			if i == 0 && j == 0 {
-				continue
-			}
-			minimum := math.MaxInt
-			if i > 0 {
-				minimum = minRisk[i-1][j] + grid[i][j]
-			}
-			if j > 0 && minRisk[i][j-1]+grid[i][j] < minimum {
-				minimum = minRisk[i][j-1] + grid[i][j]
-			}
-			minRisk[i][j] = minimum
+func part2(grid [][]int) {
+	// we'll need to repeat the grid 8 times and append 4 times to the right,
+	// then append 4 times below
+	tiles := make([][][]int, 9)
+	tiles[0] = grid
+	for i := 1; i < 9; i++ {
+		if i == 0 {
+			tiles[i] = repeatGrid(grid)
+		} else {
+			tiles[i] = repeatGrid(tiles[i-1])
 		}
 	}
 
-	lastRow := len(minRisk) - 1
-	lastCol := len(minRisk[0]) - 1
-	fmt.Println("part 1: ", minRisk[lastRow][lastCol])
+	rows := make([][][]int, 5)
+	for i := range rows {
+		row := tiles[i]
+		for j := i + 1; j < i+5; j++ {
+			for r := 0; r < len(row); r++ {
+				row[r] = append(row[r], tiles[j][r]...)
+			}
+		}
+		rows[i] = row
+	}
+
+	var fullGrid [][]int
+	for _, row := range rows {
+		fullGrid = append(fullGrid, row...)
+	}
+
+	// fmt.Println("len fullgrid: ", len(fullGrid))
+	// fmt.Println("len fullgrid[0]: ", len(fullGrid[0]))
+	// for _, r := range fullGrid {
+	// 	fmt.Println(r)
+	// }
+
+	fmt.Println("part 2: ", minRisk(fullGrid))
+}
+
+// repeatGrid creates a copy of grid with each value incremented by 1,
+// wrapping values of 9 back around to 1
+func repeatGrid(grid [][]int) [][]int {
+	repeated := make([][]int, len(grid))
+	for i := range repeated {
+		repeated[i] = make([]int, len(grid[i]))
+		for j := 0; j < len(repeated[i]); j++ {
+			repeated[i][j] = grid[i][j] + 1
+			if repeated[i][j] > 9 {
+				repeated[i][j] = 1
+			}
+		}
+	}
+
+	return repeated
+}
+
+type Position struct {
+	Row, Col int
+}
+
+func (p Position) DecRow() Position {
+	return Position{p.Row - 1, p.Col}
+}
+
+func (p Position) DecCol() Position {
+	return Position{p.Row, p.Col - 1}
+}
+
+func (p Position) IncRow() Position {
+	return Position{p.Row + 1, p.Col}
+}
+
+func (p Position) IncCol() Position {
+	return Position{p.Row, p.Col + 1}
+}
+
+type Grid [][]int
+
+func (g Grid) Adjacent(pos Position) []Position {
+	var adjacent []Position
+	if pos.Row > 0 {
+		adjacent = append(adjacent, Position{pos.Row - 1, pos.Col})
+	}
+	if pos.Col > 0 {
+		adjacent = append(adjacent, Position{pos.Row, pos.Col - 1})
+	}
+	if pos.Row < len(g)-1 {
+		adjacent = append(adjacent, Position{pos.Row + 1, pos.Col})
+	}
+	if pos.Col < len(g[pos.Row])-1 {
+		adjacent = append(adjacent, Position{pos.Row, pos.Col + 1})
+	}
+	return adjacent
+}
+
+type Risk struct {
+	Pos     Position
+	MinRisk int
+}
+
+type RiskHeap []Risk
+
+func (h RiskHeap) Len() int           { return len(h) }
+func (h RiskHeap) Less(i, j int) bool { return h[i].MinRisk < h[j].MinRisk }
+func (h RiskHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *RiskHeap) Push(x interface{}) {
+	*h = append(*h, x.(Risk))
+}
+
+func (h *RiskHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func minRisk(grid Grid) int {
+	h := &RiskHeap{Risk{Position{0, 0}, 0}}
+	heap.Init(h)
+	visited := make(map[Position]bool)
+	dest := Position{len(grid) - 1, len(grid[0]) - 1}
+
+	for {
+		riskPos := heap.Pop(h).(Risk)
+		if visited[riskPos.Pos] {
+			continue
+		}
+		visited[riskPos.Pos] = true
+
+		if riskPos.Pos == dest {
+			return riskPos.MinRisk
+		}
+
+		for _, adj := range grid.Adjacent(riskPos.Pos) {
+			if visited[adj] {
+				continue
+			}
+
+			heap.Push(h, Risk{adj, riskPos.MinRisk + grid[adj.Row][adj.Col]})
+		}
+	}
 }
 
 func readInput() [][]int {
