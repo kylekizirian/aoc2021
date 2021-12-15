9323c42ec91166672e13aaee0e7ab7f6342e1e2d
Kyle Kizirian <kyle.kizirian@gmail.com>
2021-12-14 22:13:45 -0500

    day 14 part 2

--- day14/main.go
+++ day14/main.go
@@ -11,11 +11,12 @@ import (
 func main() {
 	template, rules := readInput()
 	part1(template, rules)
+	part2(template, rules)
 }
 
 func part1(template string, rules map[string]string) {
 	current := template
-	for i := 1; i < 11; i++ {
+	for i := 0; i < 10; i++ {
 		next := ""
 		for j := 0; j < len(current)-1; j++ {
 			first := string(current[j])
@@ -35,17 +36,38 @@ func part1(template string, rules map[string]string) {
 		ctr[r]++
 	}
 
-	min, max := math.MaxInt, math.MinInt
-	for _, val := range ctr {
-		if val < min {
-			min = val
-		}
-		if val > max {
-			max = val
-		}
+	min, max := minAndMax(ctr)
+	fmt.Println("part1: ", max-min)
+}
+
+func part2(template string, rules map[string]string) {
+	pairs := make(map[string]int)
+	for i := 0; i < len(template)-1; i++ {
+		substr := string(template[i]) + string(template[i+1])
+		pairs[substr]++
 	}
 
-	fmt.Println("part1: ", max-min)
+	for i := 0; i < 40; i++ {
+		nextPairs := make(map[string]int)
+		for pair, count := range pairs {
+			insertion := rules[pair]
+			firstSubstr := string(pair[0]) + insertion
+			secondSubstr := insertion + string(pair[1])
+			nextPairs[firstSubstr] += count
+			nextPairs[secondSubstr] += count
+		}
+		pairs = nextPairs
+	}
+
+	ctr := make(map[rune]int)
+	for substr, count := range pairs {
+		ctr[rune(substr[0])] += count
+	}
+	// account for last character in template string
+	ctr[rune(template[len(template)-1])]++
+
+	min, max := minAndMax(ctr)
+	fmt.Println("part2: ", max-min)
 }
 
 type Node struct {
@@ -64,6 +86,20 @@ func (n *Node) String() string {
 		cur = n.Next
 	}
 	return str + string(n.Char)
+}
+
+// minAndMax returns the minimum and maximum values in ctr map
+func minAndMax(ctr map[rune]int) (int, int) {
+	min, max := math.MaxInt, math.MinInt
+	for _, val := range ctr {
+		if val < min {
+			min = val
+		}
+		if val > max {
+			max = val
+		}
+	}
+	return min, max
 }
 
 func readInput() (string, map[string]string) {
